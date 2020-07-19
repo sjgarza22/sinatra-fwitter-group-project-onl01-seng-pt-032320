b68632e03e6a258a050531265bd61fb765c255f8
sjgarza22 <sjgarza22@gmail.com>
2020-07-19 00:48:20 -0500

    Enable Sessions & add Get for Index page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,11 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions unless test?
+    set :session_secret, "7b4ca3aea1b27763e3f7079872275c8f"
   end
 
+  get '/' do
+    erb :index
+  end
 end
